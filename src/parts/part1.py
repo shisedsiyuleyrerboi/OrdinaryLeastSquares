6d7877b52e763fc4c0a10b8ec76b048971205cb6
Francisco Luna Ratto <franciscolunaratto@icloud.com>
2025-12-01 17:36:51 -0500

    Delete src directory

--- src/parts/part1.py
+++
@@ -1,9 +0,0 @@
-from variables import *
-
-values_p1 = [0, 0.2]
-
-prob_1 = stats.norm.cdf(values_p1[0], loc=mean, scale=sd)
-prob_2 = 1 - stats.norm.cdf(values_p1[1], loc=mean, scale=sd)
-
-print(f'P(R < 0.0)   = {prob_1:.4f}')
-print(f'P(R > 0.2)   = {prob_2:.4f}')
